95845952a6859bbd8d98faeaa64e40f59cfc9822
Andres Rivero <andres19rivero@gmail.com>
2022-07-29 21:08:28 -0400

    sidemenu navigation routes

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  text: string;
+  route: string;
+}
+
 @Component({
   selector: 'app-sidemenu',
   templateUrl: './sidemenu.component.html',
-  styleUrls: ['./sidemenu.component.css']
+  styleUrls: ['./sidemenu.component.css'],
 })
 export class SidemenuComponent implements OnInit {
+  templateMenu: MenuItem[] = [
+    { text: 'Basics', route: './template/basics' },
+    { text: 'Dynamics', route: './template/dynamics' },
+    { text: 'Switches', route: './template/switches' },
+  ];
 
-  constructor() { }
+  reactiveMenu: MenuItem[] = [
+    { text: 'Basics', route: './reactive/basics' },
+    { text: 'Dynamics', route: './reactive/dynamics' },
+    { text: 'Switches', route: './reactive/switches' },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
