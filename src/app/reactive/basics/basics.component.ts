4771152291ec061c8822620f69604309b18964dc
Andres Rivero <andres19rivero@gmail.com>
2022-08-01 22:06:36 -0400

    bind form group to template form

--- src/app/reactive/basics/basics.component.ts
+++ src/app/reactive/basics/basics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-basics',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./basics.component.css']
 })
 export class BasicsComponent implements OnInit {
+  myForm: FormGroup = new FormGroup({
+    'name': new FormControl('')
+  })
 
   constructor() { }
 
