5c4ed69495f034874480fe290c18f025bb5a2367
Andres Rivero <andres19rivero@gmail.com>
2022-08-04 22:50:08 -0400

    created the form with the name input control

--- src/app/reactive/dynamics/dynamics.component.ts
+++ src/app/reactive/dynamics/dynamics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dynamics',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dynamics.component.css']
 })
 export class DynamicsComponent implements OnInit {
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]]
+  })
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors
+      && this.myForm.controls[field].touched
+  }
+
+  save(): void {
+    if(this.myForm.invalid) {
+      this.myForm.markAllAsTouched()
+      return
+    }
+    console.log(this.myForm.value)
+    this.myForm.reset()
+  }
 }
