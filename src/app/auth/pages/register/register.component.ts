e063a1338d06d61c839df8644a5c68220807d6ac
Andres Rivero <andres19rivero@gmail.com>
2022-08-11 22:43:50 -0400

    validate the name field with a regex

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // TODO: Temporal
+  firstLastNamesPattern: string = "([a-zA-Z]+) ([a-zA-Z]+)"
+
+  myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.pattern(this.firstLastNamesPattern)]]
+  })
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  notValidField(field: string) {
+    return this.myForm.get(field)?.invalid && this.myForm.get(field)?.touched
+  }
+
+  submitForm() {
+    console.log(this.myForm.value)
+    this.myForm.markAllAsTouched()
+  }
 }
